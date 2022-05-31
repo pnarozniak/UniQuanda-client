4e5ad5cb711741f41e343e0a152e200746b323af
xlegodroit <xxvege@gmail.com>
2022-05-31 20:17:30 +0200

    Added services to core providers

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,8 @@
 import { NgModule } from '@angular/core';
+import { TokensService } from './services/tokens.service';
+import { UserDataService } from './services/user-data.service';
 
-@NgModule({})
+@NgModule({
+	providers: [TokensService, UserDataService],
+})
 export class CoreModule {}
