3f9c0d886e4b51cb3b2447b9ff6573465327a36d
xlegodroit <xxvege@gmail.com>
2022-09-22 14:13:56 +0300

    Added HeaderDropdownComponent

---
+++ src/app/core/components/header-dropdown/header-dropdown.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUserClaims } from '../../models/user-claims.model';
+import { ThemeService } from '../../services/theme.service';
+import { UserDataService } from '../../services/user-data.service';
+
+@Component({
+	selector: 'app-header-dropdown',
+	templateUrl: './header-dropdown.component.html',
+	styleUrls: ['./header-dropdown.component.scss'],
+})
+export class HeaderDropdownComponent {
+	user$: Observable<IUserClaims | null> = this._userDataService.getUserData$();
+	isDarkTheme$: Observable<boolean> = this._themeService.isDark$();
+
+	constructor(
+		private readonly _themeService: ThemeService,
+		private readonly _userDataService: UserDataService
+	) {}
+
+	handleThemeChange(): void {
+		this._themeService.toggleTheme();
+	}
+
+	handleLogout(): void {
+		this._userDataService.clearUserData();
+	}
+}
