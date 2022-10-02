87a780ead00b4a3cd0be67702b297e295f039e37
Piotr Narożniak <53837994+xlegodroit@users.noreply.github.com>
2022-10-03 00:21:48 +0300

    Feature/QUAN-115 (#19)
    * Replaced StorageService
    
    * Added ThemeService
    
    * Added ThemeDirective
    
    * Added HeaderDropdownComponent
    
    * Added docs to UserDataService
    
    * Added initial state loading to ThemeService
    
    * fixed register
    
    * Removed logout button on mobile view
    
    * Fixed user-state loading after refresh
    
    * Changed images location
    
    * Added dropdown close-icon
    
    * Fixed search-btn
    
    * Changed budgets
    
    * Fixed issues mentioned in CR
    
    * Moved google_login image to images folder
    
    * Fixed tabs
    
    * Fixed issues mentioned in CR

--- src/app/core/components/nav-bar/nav-bar.component.ts
+++ src/app/core/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
 import { UserDataService } from 'src/app/core/services/user-data.service';
 import { Component } from '@angular/core';
+import { IUserClaims } from '../../models/user-claims.model';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-nav-bar',
@@ -7,5 +9,7 @@ import { Component } from '@angular/core';
 	styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+	user$: Observable<IUserClaims | null> = this._userDataService.getUserData$();
+
 	constructor(public readonly _userDataService: UserDataService) {}
 }
