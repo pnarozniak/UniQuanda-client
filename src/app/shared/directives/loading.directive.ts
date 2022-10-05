f6cfc665871c0c64f5999f97970a5da7e4daf855
Dmitry Teplov <DmitryTeplov00@gmail.com>
2022-10-05 13:24:59 +0200

    Dodano dyrektywę ładowania i dodano ładowanie zdjęcia i bannera

---
+++ src/app/shared/directives/loading.directive.ts
@@ -0,0 +1,50 @@
+import {
+	Directive,
+	ElementRef,
+	HostBinding,
+	Input,
+	OnChanges,
+	Renderer2,
+	SimpleChanges,
+} from '@angular/core';
+
+@Directive({
+	selector: '[appLoading]',
+})
+export class LoadingDirective implements OnChanges {
+	private readonly _loadingClassName = 'loading';
+	@Input() appLoading = false;
+	@HostBinding('class')
+	public loadingClass = this._loadingClassName;
+	private imageRef!: any;
+
+	constructor(
+		private readonly _el: ElementRef,
+		private readonly _renderer: Renderer2
+	) {
+		this.loadingClass = this.appLoading ? this._loadingClassName : '';
+	}
+
+	ngAfterViewInit() {
+		this.imageRef = this._el.nativeElement.querySelector('img');
+		if (this.imageRef !== undefined) {
+			if (this.appLoading) {
+				this._renderer.addClass(this.imageRef, 'd-none');
+			} else {
+				this._renderer.removeClass(this.imageRef, 'd-none');
+			}
+		}
+	}
+
+	ngOnChanges(changes: SimpleChanges): void {
+		this.loadingClass = this.appLoading ? this._loadingClassName : '';
+		console.log('changes:');
+		if (this.imageRef !== undefined) {
+			if (this.appLoading) {
+				this._renderer.addClass(this.imageRef, 'd-none');
+			} else {
+				this._renderer.removeClass(this.imageRef, 'd-none');
+			}
+		}
+	}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { LoadingDirective } from './directives/loading.directive';
 import { ThemeDirective } from './directives/theme.directive';
 
 @NgModule({
-	declarations: [ThemeDirective],
-	exports: [ThemeDirective],
+	declarations: [ThemeDirective, LoadingDirective],
+	exports: [ThemeDirective, LoadingDirective],
 })
 export class SharedModule {}
