b21887742b00ede58921b0452038009c98bf75c7
xlegodroit <xxvege@gmail.com>
2022-10-24 02:31:01 +0200

    Fixed typo in page title

--- src/app/features/public/recover-password/recover-password-routing.module.ts
+++ src/app/features/public/recover-password/recover-password-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
 		path: '',
 		pathMatch: 'full',
 		component: RecoverPasswordComponent,
-		data: { title: 'Odyskaj hasło' },
+		data: { title: 'Odzyskaj hasło' },
 	},
 ];
 @NgModule({
