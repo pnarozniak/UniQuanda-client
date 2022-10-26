98677c59e068aa69f2d57f8840ed2ecffceae792
Patrykoss <adresikps@gmail.com>
2022-10-26 23:46:12 +0200

    Added preview password content

---
+++ src/app/features/user/user-settings/pages/security-settings/components/password-content/password-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-content',
+  templateUrl: './password-content.component.html',
+  styleUrls: ['./password-content.component.scss']
+})
+export class PasswordContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
