9d466aef418f5baba92a40f5faf93d829f6312a6
Patrykoss <adresikps@gmail.com>
2022-11-13 13:13:14 +0100

    Updated action of change main email

--- src/app/features/user/user-settings/pages/security-settings/components/main-email-content/main-email-content.component.ts
+++ src/app/features/user/user-settings/pages/security-settings/components/main-email-content/main-email-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { IGetUserEmailsReponseDTO } from '../../models/get-user-emails-reponse.dto';
 import { ScrollToElementFeatureComponent } from '../scroll-to-element-feature/scroll-to-element-feature.component';
 
@@ -13,10 +14,20 @@ import { ScrollToElementFeatureComponent } from '../scroll-to-element-feature/sc
 export class MainEmailContentComponent extends ScrollToElementFeatureComponent {
 	@Input() userEmails: IGetUserEmailsReponseDTO | null = null;
 
+	constructor(private readonly _toastrService: ToastrService) {
+		super();
+	}
 	isFormVisible = false;
 
 	changeVisibilityForm() {
-		this.isFormVisible = !this.isFormVisible;
-		super.scrollToEl();
+		if (!this.isFormVisible && this.userEmails?.emailToConfirm) {
+			this._toastrService.error(
+				'Zmiana wymaga potwierdzenie innego e-maila',
+				'Niedostępna akcja'
+			);
+		} else {
+			this.isFormVisible = !this.isFormVisible;
+			super.scrollToEl();
+		}
 	}
 }
