06060c7f32553a03c4317d0eabad830897c31d3f
Patrykoss <adresikps@gmail.com>
2022-11-17 00:47:21 +0100

    Added disabled to app button

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -17,13 +17,16 @@ export class ButtonComponent {
 	@Input() color: 'none' | 'blue' | 'purple' | 'gray' = 'blue';
 	@Input() shape: 'round' | 'rect' = 'round';
 	@ViewChild('buttonRef') buttonRef?: ElementRef;
+	@Input() disabled: boolean = false;
 
 	@HostBinding('class') get buttonClasses() {
-		return [this.color, this.shape].map((c) => `app-button-${c}`).join(' ');
+		return [this.color, this.shape, this.disabled ? 'disabled' : 'enabled'].map((c) => `app-button-${c}`).join(' ');
 	}
 
 	@HostListener('click')
 	onClick() {
-		this.buttonRef?.nativeElement.click();
+		if(!this.disabled) {
+			this.buttonRef?.nativeElement.click();
+		}
 	}
 }
