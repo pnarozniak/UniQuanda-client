afd37fa1e3056bbe0057060951d88f133d89b941
xlegodroit <xxvege@gmail.com>
2022-11-27 19:32:48 +0100

    Configured env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,4 +4,11 @@ export const environment = {
 	...common,
 	productionMode: true,
 	backend: 'https://dev.uniquanda.pl:2002/api',
+	oauth: {
+		...common.oauth,
+		google: {
+			...common.oauth.google,
+			redirectUrl: 'https://uniquanda.pl:2002/api/Auth/login-by-google',
+		},
+	},
 };
