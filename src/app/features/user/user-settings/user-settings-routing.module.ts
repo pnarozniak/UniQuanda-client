0ba641a4597b35eccbbc541a3c36fddccaa8bf58
Patrykoss <adresikps@gmail.com>
2022-11-28 17:31:57 +0100

    Squashed commit of the following:
    commit 5df736dc19860fd41225162a3a887fe7f5a90e66
    Author: Piotr Narożniak <53837994+xlegodroit@users.noreply.github.com>
    Date:   Mon Nov 28 01:06:35 2022 +0100
    
        feature/QUAN-146 | Google login (#46)
    
        * Added login by google
    
        * Configured env
    
        * Fix
    
        * fix#2
    
        * Roles fix
    
    commit cd6d2cf4478b627ad37b6e99530b333e5688958b
    Author: Patrykoss <adresikps@gmail.com>
    Date:   Sat Nov 26 11:13:35 2022 +0100
    
        Quick fix rollbacked commit

--- src/app/features/user/user-settings/user-settings-routing.module.ts
+++ src/app/features/user/user-settings/user-settings-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserSettingsComponent } from './user-settings.component';
+import AuthGuardService from 'src/app/core/guards/auth-guard.service';
 
 const routes: Routes = [
 	{
