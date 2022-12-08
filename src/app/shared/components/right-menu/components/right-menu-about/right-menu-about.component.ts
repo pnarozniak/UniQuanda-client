945d3e67f6c33284da9e94ae22560da5c36c36f8
xlegodroit <xxvege@gmail.com>
2022-12-08 02:11:30 +0100

    Added right-menu-about component

---
+++ src/app/shared/components/right-menu/components/right-menu-about/right-menu-about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-right-menu-about',
+	templateUrl: './right-menu-about.component.html',
+	styleUrls: ['./right-menu-about.component.scss'],
+})
+export class RightMenuAboutComponent {}
