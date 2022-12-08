b379023dfbd3a7f1cf57e58279c332c548f7d9f6
xlegodroit <xxvege@gmail.com>
2022-12-08 02:11:52 +0100

    Added right-menu-buy-premium component

---
+++ src/app/shared/components/right-menu/components/right-menu-buy-premium/right-menu-buy-premium.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-right-menu-buy-premium',
+	templateUrl: './right-menu-buy-premium.component.html',
+	styleUrls: ['./right-menu-buy-premium.component.scss'],
+})
+export class RightMenuBuyPremiumComponent {}
