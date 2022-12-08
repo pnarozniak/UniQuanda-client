ab366c3fc0108fbe87e8031f363d194a0d88d037
xlegodroit <xxvege@gmail.com>
2022-12-08 02:12:16 +0100

    Added right-menu-create-an-account component

---
+++ src/app/shared/components/right-menu/components/right-menu-create-an-account/right-menu-create-an-account.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-right-menu-create-an-account',
+	templateUrl: './right-menu-create-an-account.component.html',
+	styleUrls: ['./right-menu-create-an-account.component.scss'],
+})
+export class RightMenuCreateAnAccountComponent {}
