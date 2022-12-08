67a9a9ed5bd6758510047d9fcc68f7c5874864a5
Dmitry Teplov <DmitryTeplov00@gmail.com>
2022-12-08 19:54:29 +0100

    dodano wyświetlanie listy pytań

--- src/app/features/public/home/home.module.ts
+++ src/app/features/public/home/home.module.ts
@@ -6,8 +6,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule } from '@angular/forms';
 import { QuestionBoxComponent } from './components/question-box/question-box.component';
 import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
-	imports: [CommonModule, SharedModule, FormsModule, RouterModule],
+	imports: [
+		CommonModule,
+		SharedModule,
+		FormsModule,
+		RouterModule,
+		MatMenuModule,
+	],
 	declarations: [HomeComponent, QuestionSortingComponent, QuestionBoxComponent],
 })
 export class HomeModule {}
