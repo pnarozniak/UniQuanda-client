c873726eca96adea30365e9fed76b9000c78336d
Dmitry "Ventan" Teplov <56700157+Ventan00@users.noreply.github.com>
2022-12-16 14:19:34 +0100

    usunięto resztki reklam i poprawiono teksty premium (#58)

--- src/app/shared/components/ad-containers/ad.d.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/naming-convention
-declare interface Window {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	adsbygoogle: any[];
-}
-
-// eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
-declare var adsbygoogle: any[];
