f7a9ef55c5e2ef3fe5e71cdc880d60c69f818341
Dmitry Teplov <DmitryTeplov00@gmail.com>
2022-12-20 01:42:02 +0100

    Dodano limity

--- src/app/features/user/ask-question/ask-question.component.ts
+++ src/app/features/user/ask-question/ask-question.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LoaderService } from 'src/app/core/services/loader.service';
+import { DurationEnum } from 'src/app/shared/enums/duration.enum';
 import { ITag } from 'src/app/shared/models/tag.model';
 import AddQuestionRequestDTO from './models/add-question.dto';
 import AskQuestionApiService from './services/ask-question-api.service';
@@ -12,13 +13,14 @@ import AskQuestionApiService from './services/ask-question-api.service';
 	templateUrl: './ask-question.component.html',
 	styleUrls: ['./ask-question.component.scss'],
 })
-export class AskQuestionComponent {
+export class AskQuestionComponent implements OnInit {
 	public form = new FormGroup({
 		title: new FormControl('', [Validators.required]),
 		content: new FormControl('', [Validators.required]),
 		confirmation: new FormControl(false, [Validators.requiredTrue]),
 	});
 	public maxTagsAmount = 5;
+	public finishedInitialLoading = false;
 
 	public constructor(
 		private readonly _askQuestionApiService: AskQuestionApiService,
@@ -26,6 +28,30 @@ export class AskQuestionComponent {
 		private readonly _toastrService: ToastrService,
 		private readonly _loaderService: LoaderService
 	) {}
+
+	ngOnInit(): void {
+		this._askQuestionApiService.checkLimits().subscribe({
+			next: (response) => {
+				if (!response.maxTimes || !response.usedTimes) {
+					this.finishedInitialLoading = true;
+					return;
+				}
+				if (response.maxTimes <= response.usedTimes) {
+					this.finishedInitialLoading = true;
+					this._router.navigate(['/user/ask-question/limit-exceeded'], {
+						queryParams: {
+							duration: this.translateDuration(
+								response.shortestRefreshPeriod as DurationEnum
+							),
+							limit: response.maxTimes,
+						},
+					});
+					return;
+				}
+				this.finishedInitialLoading = true;
+			},
+		});
+	}
 
 	public tagInput = new FormControl('');
 	public tags: ITag[] = [];
@@ -55,12 +81,27 @@ export class AskQuestionComponent {
 					'Dodano pytanie'
 				);
 				this._loaderService.hide();
-				this._router.navigate(['public/question/details', questionId]);
+				this._router.navigate(['public/question/details', questionId.body]);
+			},
+			error: (error) => {
+				if ((error.status = 403)) {
+					this._toastrService.error(
+						'Nie możesz chwilowo zadawać kolejnych pytań',
+						'Wyczerpano limit pytań'
+					);
+				} else {
+					this._toastrService.error('Nie udało się dodać pytania', 'Błąd');
+				}
 			},
 		});
 	}
 
 	public setTags(tags: ITag[]) {
 		this.tags = tags;
+	}
+
+	private translateDuration(duration: DurationEnum): string {
+		if (duration === DurationEnum.Day) return 'Day';
+		return 'Week';
 	}
 }
--- src/app/features/user/ask-question/ask-question.routing.module.ts
+++ src/app/features/user/ask-question/ask-question.routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AskQuestionComponent } from './ask-question.component';
+import { LimitExceededComponent } from './pages/limit-exceeded/limit-exceeded.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: AskQuestionComponent,
 		data: { title: 'Zadaj pytanie' },
+	},
+	{
+		path: 'limit-exceeded',
+		component: LimitExceededComponent,
+		data: { title: 'Limit zadawania pytań przekroczony' },
 	},
 ];
 
