abc7b2eb102dab9d83bd9a5a40517e311310c88a
Dmitry Teplov <DmitryTeplov00@gmail.com>
2022-12-20 16:57:17 +0100

    quan-188/quan-190/quan-191/quan-194/quan-183/quan-184/quan-186/quan-176/quan-177

--- src/app/features/user/ask-question/ask-question.component.ts
+++ src/app/features/user/ask-question/ask-question.component.ts
@@ -17,7 +17,11 @@ import AskQuestionApiService from './services/ask-question-api.service';
 })
 export class AskQuestionComponent implements OnInit {
 	public form = new FormGroup({
-		title: new FormControl('', [Validators.required]),
+		title: new FormControl('', [
+			Validators.required,
+			Validators.minLength(5),
+			Validators.maxLength(300),
+		]),
 		content: new FormControl('', [Validators.required]),
 		confirmation: new FormControl(false, [Validators.requiredTrue]),
 	});
