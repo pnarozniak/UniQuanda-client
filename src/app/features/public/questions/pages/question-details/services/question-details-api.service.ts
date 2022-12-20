6f07221fc7a32a91ca48622a6af0c28947d9ae8b
Patrykoss <adresikps@gmail.com>
2022-12-20 19:24:08 +0100

    Updated docs

--- src/app/features/public/questions/pages/question-details/services/question-details-api.service.ts
+++ src/app/features/public/questions/pages/question-details/services/question-details-api.service.ts
@@ -9,22 +9,39 @@ import { IDeleteQuestionResponseDTO } from '../models/delete-question.dto';
 export class QuestionDetailsApiService {
 	constructor(private readonly _apiService: ApiService) {}
 
+	/**
+	 * Send request to get question details
+	 * @returns Observable<HttpResponse<IQuestionDetailsResponseDTO>> object with status code of request
+	 * and data of IQuestionDetailsResponseDTO
+	 */
 	public getQuestionDetails(
 		idQuestion: number
 	): Observable<HttpResponse<IQuestionDetailsResponseDTO>> {
 		return this._apiService.get(`Question/${idQuestion}`);
 	}
 
+	/**
+	 * Send request to update follow question info
+	 * @returns Observable<HttpResponse<null>> object with status code of request
+	 */
 	public followQuestion(idQuestion: number): Observable<HttpResponse<null>> {
 		return this._apiService.put(`Question/follow/${idQuestion}`, null);
 	}
 
+	/**
+	 * Send request to delete question
+	 * @returns Observable<HttpResponse<null>> object with status code of request
+	 */
 	public deleteQuestion(
 		idQuestion: number
 	): Observable<HttpResponse<IDeleteQuestionResponseDTO | null>> {
 		return this._apiService.delete(`Question/${idQuestion}`);
 	}
 
+	/**
+	 * Send request to update question views
+	 * @returns Observable<HttpResponse<null>> object with status code of request
+	 */
 	public updateViews(idQuestion: number): Observable<HttpResponse<null>> {
 		return this._apiService.put(`Question/Views/${idQuestion}`, null);
 	}
