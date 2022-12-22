cef116c365538a79a4520d7f47bb3267fd6145ed
Piotr Narożniak <53837994+xlegodroit@users.noreply.github.com>
2022-12-22 15:25:39 +0100

    Fix (#69)

--- src/app/shared/components/right-menu/components/right-menu-generate-test/right-menu-generate-test.component.ts
+++ src/app/shared/components/right-menu/components/right-menu-generate-test/right-menu-generate-test.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { DialogService } from 'src/app/core/services/dialog.service';
+import { LoaderService } from 'src/app/core/services/loader.service';
+import { UserDataService } from 'src/app/core/services/user-data.service';
+import { TestsDashboardApiService } from 'src/app/features/user/test/tests-dashboard/services/tests-dashboard-api.service';
 import { ITag } from 'src/app/shared/models/tag.model';
+import { CreateAnAccountDialogComponent } from '../../../dialogs/create-an-account-dialog/create-an-account-dialog.component';
 
 @Component({
 	selector: 'app-right-menu-generate-test',
@@ -12,18 +18,40 @@ export class RightMenuGenerateTestComponent {
 	control = new FormControl('');
 	selectedTags: ITag[] = [];
 
-	constructor(private router: Router) {}
+	constructor(
+		private router: Router,
+		private testsDashboardApi: TestsDashboardApiService,
+		private loader: LoaderService,
+		private toastr: ToastrService,
+		private userData: UserDataService,
+		private dialog: DialogService
+	) {}
 
 	generateTest() {
+		if (!this.userData.getUserData()) {
+			this.dialog.open(CreateAnAccountDialogComponent);
+			return;
+		}
+
 		this.control.setErrors(
 			this.selectedTags.length === 0 ? { required: true } : null
 		);
 		if (this.control.invalid) return;
 
-		this.router.navigate(['/user/test/automatic'], {
-			queryParams: {
-				tagIds: this.selectedTags.map((t) => t.id).join(','),
-			},
-		});
+		this.loader.show();
+		this.testsDashboardApi
+			.generateTest$(this.selectedTags.map((t) => t.id))
+			.subscribe({
+				next: (testId) => {
+					this.router.navigate(['/user/test/', testId]);
+				},
+				error: () => {
+					this.loader.hide();
+					this.toastr.error(
+						'Przepraszamy, nie mogliśmy wygenerować testu dla wybranych tagów.',
+						'Błąd generowania testu.'
+					);
+				},
+			});
 	}
 }
