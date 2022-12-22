d8a0d6cd4b5ccdf7bc0b789b0c9b995c155946f9
Patrykoss <adresikps@gmail.com>
2022-12-22 16:13:13 +0100

    Updated display comments

--- src/app/features/public/questions/pages/question-details/components/answer-details/answer-details.component.ts
+++ src/app/features/public/questions/pages/question-details/components/answer-details/answer-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { finalize } from 'rxjs';
+import { LoaderService } from 'src/app/core/services/loader.service';
 import { IAnswerDetails } from '../../models/answer-details.dto';
 import { IQuestionDetailsEntity } from '../../models/question-details.dto';
+import { AnswersApiService } from '../../services/answers-api.service';
 
 @Component({
 	selector: 'app-answer-details',
@@ -18,8 +21,15 @@ export class AnswerDetailsComponent implements OnInit {
 	@Input() answerToScroll: number | null = null;
 	@Input() commentToScroll: number | null = null;
 	@Input() customId = '';
+	@Input() i!: number;
 
-	isContentOwner = false;
+	public isContentOwner = false;
+	public isCommentsDisabled = true;
+
+	constructor(
+		private readonly _answersApiService: AnswersApiService,
+		private readonly _loader: LoaderService
+	) {}
 
 	ngOnInit(): void {
 		this.isContentOwner = this.answer.author.id === this.idLoggedUser;
@@ -27,14 +37,37 @@ export class AnswerDetailsComponent implements OnInit {
 			const el = document.getElementById(`comment${this.commentToScroll}`);
 			el?.scrollIntoView({
 				behavior: 'smooth',
-				block: 'start',
+				block: 'center',
 			});
 		} else if (this.answerToScroll && !this.commentToScroll) {
 			const el = document.getElementById(`item${this.answerToScroll}`);
 			el?.scrollIntoView({
 				behavior: 'smooth',
-				block: 'start',
+				block: 'center',
 			});
 		}
+	}
+
+	updateCommentsVisibility(): void {
+		this.isCommentsDisabled = !this.isCommentsDisabled;
+	}
+
+	public generateCommentsCountText(commentsAmount: number): string {
+		if (commentsAmount === 1) {
+			return '1 odpowiedź';
+		}
+		return `${commentsAmount} odpowiedzi`;
+	}
+
+	getAllComments(answer: IAnswerDetails): void {
+		this._loader.show();
+		this._answersApiService
+			.getAllComments(answer.id)
+			.pipe(finalize(() => this._loader.hide()))
+			.subscribe({
+				next: (res) => {
+					answer.comments = res.body!.comments;
+				},
+			});
 	}
 }
