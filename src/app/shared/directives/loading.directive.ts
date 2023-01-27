320c1ecd0865d9ff39ff9efeded3afb4c03ddee2
Dmitry Teplov <DmitryTeplov00@gmail.com>
2023-01-27 23:14:42 +0100

    dyrektywa loading.directive.ts

--- src/app/shared/directives/loading.directive.ts
+++ src/app/shared/directives/loading.directive.ts
@@ -5,6 +5,7 @@ import {
 	HostBinding,
 	Input,
 	OnChanges,
+	OnInit,
 	Renderer2,
 	SimpleChanges,
 } from '@angular/core';
@@ -12,39 +13,54 @@ import {
 @Directive({
 	selector: '[appLoading]',
 })
-export class LoadingDirective implements OnChanges, AfterViewInit {
+export class LoadingDirective implements OnChanges, OnInit {
 	private readonly _loadingClassName = 'loading';
 	@Input() appLoading = false;
 	@HostBinding('class')
-	public loadingClass = this._loadingClassName;
+	public loadingClass = '';
+
 	private imageRef!: any;
+	private interval: any = null;
 
 	constructor(
 		private readonly _el: ElementRef,
 		private readonly _renderer: Renderer2
-	) {
-		this.loadingClass = this.appLoading ? this._loadingClassName : '';
-	}
+	) {}
 
-	ngAfterViewInit() {
+	ngOnInit() {
 		this.imageRef = this._el.nativeElement.querySelector('img');
-		if (this.imageRef !== undefined && this.imageRef !== null) {
-			if (this.appLoading) {
+		if (this.appLoading) {
+			if (this.imageRef !== undefined && this.imageRef !== null) {
 				this._renderer.addClass(this.imageRef, 'd-none');
-			} else {
-				this._renderer.removeClass(this.imageRef, 'd-none');
 			}
+			this.addInterval();
 		}
 	}
 
 	ngOnChanges(changes: SimpleChanges): void {
-		this.loadingClass = this.appLoading ? this._loadingClassName : '';
-		if (this.imageRef !== undefined && this.imageRef !== null) {
-			if (this.appLoading) {
-				this._renderer.addClass(this.imageRef, 'd-none');
-			} else {
+		clearTimeout(this.interval);
+		if (this.appLoading) {
+			this.addInterval();
+		} else {
+			this.loadingClass = '';
+			if (this.imageRef !== undefined && this.imageRef !== null) {
 				this._renderer.removeClass(this.imageRef, 'd-none');
 			}
 		}
+	}
+
+	private addInterval() {
+		this.interval = setTimeout(() => {
+			if (this.appLoading) {
+				this.loadingClass = this._loadingClassName;
+				if (
+					this.imageRef !== undefined &&
+					this.imageRef !== null &&
+					this.appLoading
+				) {
+					this._renderer.addClass(this.imageRef, 'd-none');
+				}
+			}
+		}, 500);
 	}
 }
